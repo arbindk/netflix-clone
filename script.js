01381a17e1e07693debc29cd68de465805934a6a
Amarjeet Singh <amarjeet.singh@accops.com>
2020-10-27 19:56:43 +0530

    added trailer part

--- script.js
+++ script.js
@@ -1,41 +1,100 @@
+// Called whe the page is loaded
 window.onload = () => {
-  fetchMovies();
+  getOriginals();
+  getTrendingNow();
+  getTopRated();
 };
 
-//fetching movies from API
+async function getMovieTrailer(id) {
+  var url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`;
+  return await fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("something went wrong");
+    }
+  });
+}
 
-function fetchMovies() {
-  fetch(
-    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213"
-  )
+const setTrailer = (trailers) => {
+  const iframe = document.getElementById("movieTrailer");
+  const movieNotFound = document.querySelector(".movieNotFound");
+  if (trailers.length > 0) {
+    movieNotFound.classList.add("d-none");
+    iframe.classList.remove("d-none");
+    iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
+  } else {
+    iframe.classList.add("d-none");
+    movieNotFound.classList.remove("d-none");
+  }
+};
+
+const handleMovieSelection = (e) => {
+  const id = e.target.getAttribute("data-id");
+  const iframe = document.getElementById("movieTrailer");
+  // here we need the id of the movie
+  getMovieTrailer(id).then((data) => {
+    const results = data.results;
+    const youtubeTrailers = results.filter((result) => {
+      if (result.site == "YouTube" && result.type == "Trailer") {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    setTrailer(youtubeTrailers);
+  });
+
+  // open modal
+  $("#trailerModal").modal("show");
+  // we need to call the api with the ID
+};
+
+function showMovies(movies, element_selector, path_type) {
+  var moviesEl = document.querySelector(element_selector);
+  for (var movie of movies.results) {
+    var imageElement = document.createElement("img");
+    imageElement.setAttribute("data-id", movie.id);
+    imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`;
+
+    imageElement.addEventListener("click", (e) => {
+      handleMovieSelection(e);
+    });
+    moviesEl.appendChild(imageElement);
+  }
+}
+
+function fetchMovies(url, element_selector, path_type) {
+  fetch(url)
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        throw new error("Something went wrong");
+        throw new Error("something went wrong");
       }
     })
     .then((data) => {
-      addMovies(data);
+      showMovies(data, element_selector, path_type);
     })
     .catch((error_data) => {
       console.log(error_data);
     });
 }
 
-//Add movies to the front end
+function getOriginals() {
+  var url =
+    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213";
+  fetchMovies(url, ".original__movies", "poster_path");
+}
 
-function addMovies(movies) {
-  //<img src="https://image.tmdb.org/t/p/original//scZlQQYnDVlnpxFTxaIv2g0BWnL.jpg" />;
-  // Add image to the original__movies element
+function getTrendingNow() {
+  var url =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045";
+  fetchMovies(url, "#trending", "backdrop_path");
+}
 
-  var moviesEl = document.querySelector(".original__movies");
-  console.log(moviesEl);
-
-  for (var movie of movies.results) {
-    var image = `
-      <img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></img>`;
-
-    moviesEl.innerHTML += image;
-  }
+function getTopRated() {
+  var url =
+    "https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1";
+  fetchMovies(url, "#top_rated", "backdrop_path");
 }
